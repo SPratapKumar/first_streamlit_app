27c9f0867011c918e03fe4214d90877f984b1ced
Pratap Kumar S <s.pratapkumar@live.com>
2023-02-13 10:43:28 +0530

    Added more content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast menu")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & rocket smoothie')
+streamlit.text('Hard-Boiled Free-range Egg')
